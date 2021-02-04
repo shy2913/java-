ebab7ea2c448307c6e9f896660d41667e231945f
DESKTOP-PKK3F39\admin <yohan0355@daum.net>
2021-02-04 23:14:25 +0900

    This is a java tutorial practice 1

---
+++ src/tutorial3/Main.java
@@ -0,0 +1,13 @@
+package tutorial3;
+
+public class Main {
+	public static void main(String[] args) {
+		
+	String name="John Doe";
+	System.out.println(name);
+	System.out.println(name.substring(0,3));
+	System.out.println(name.substring(3,6));
+	System.out.println(name.substring(5,8));
+}
+}
+
